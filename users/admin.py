24ba2db3bf6adc98203e6d5277f0f59fa373eba2
Yura Simonov <yusimo@yandex.ru>
2026-02-07 23:39:06 +0300

    Внесены изменения в модель пользователя и модель пользователя добавлена в Админку

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from users.models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'last_name', 'first_name')
+    list_filter = ('last_name', )
