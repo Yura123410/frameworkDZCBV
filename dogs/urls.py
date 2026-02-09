a67b5b7f3904f3068101c573b94f68b730376951
Yura Simonov <yusimo@yandex.ru>
2026-02-09 21:41:54 +0300

    - Написан dogs_list_view; - Написан url для dogs_list_view

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from dogs.views import index, breeds_list, breeds_dogs_list
+from dogs.views import index, breeds_list_view, breeds_dogs_list_view,dogs_list_view
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
-    path('breeds/', breeds_list, name='breeds'),
-    path('breeds/<int:pk>/dogs/', breeds_dogs_list, name='breeds_dogs'),
+    # breeds
+    path('breeds/', breeds_list_view, name='breeds'),
+    path('breeds/<int:pk>/dogs/', breeds_dogs_list_view, name='breeds_dogs'),
+
+    # dogs
+    path('dogs/', dogs_list_view, name='dogs_list')
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -10,18 +10,25 @@ def index(request):
     return render(request, 'dogs/index.html', context)
 
 
-def breeds_list(request):
+def breeds_list_view(request):
     context = {
         'object_list': Breed.objects.all(),
         'title': 'Питомник - Все наши породы'
     }
     return  render(request, 'dogs/breeds.html', context)
 
-def breeds_dogs_list(request, pk: int):
+def breeds_dogs_list_view(request, pk: int):
     breeds_item = Breed.objects.get(pk=pk)
     context = {
         'object_list': Dog.objects.filter(breed_id=pk),
         'title': f'Собаки породы - {breeds_item}',
         'breed_pk': breeds_item.pk,
+    }
+    return render(request, 'dogs/dogs.html', context)
+
+def dogs_list_view(request):
+    context = {
+        'object': Dog.objects.all(),
+        'title': 'Питомник все наши собаки'
     }
     return render(request, 'dogs/dogs.html', context)
