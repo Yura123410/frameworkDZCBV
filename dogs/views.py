b2d304fe66a55087e792f5465e5ab292131eed76
Yura Simonov <yusimo@yandex.ru>
2026-02-09 21:33:13 +0300

    - Написаны breed_list view, breeds_dogs_list view; - Написаны urls для breeds_list view, breeds_dogs_list view; - Добавлены шаблоны brees.html, dogs.html

--- dogs/views.py
+++ dogs/views.py
@@ -8,3 +8,20 @@ def index(request):
         'title': 'Питомник - Главная'
     }
     return render(request, 'dogs/index.html', context)
+
+
+def breeds_list(request):
+    context = {
+        'object_list': Breed.objects.all(),
+        'title': 'Питомник - Все наши породы'
+    }
+    return  render(request, 'dogs/breeds.html', context)
+
+def breeds_dogs_list(request, pk: int):
+    breeds_item = Breed.objects.get(pk=pk)
+    context = {
+        'object_list': Dog.objects.filter(breed_id=pk),
+        'title': f'Собаки породы - {breeds_item}',
+        'breed_pk': breeds_item.pk,
+    }
+    return render(request, 'dogs/dogs.html', context)
