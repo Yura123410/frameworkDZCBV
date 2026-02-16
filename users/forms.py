70bf0dd506c9d9f58a572666a0daa02539d572f6
Yura Simonov <yusimo@yandex.ru>
2026-02-16 22:13:08 +0300

    - Написан StyleFormMixin - Добавлены фикстуры

--- users/forms.py
+++ users/forms.py
@@ -2,12 +2,20 @@ from django import forms
 
 from users.models import User
 
-class UserForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+
+class UserForm(StyleFormMixin, forms.ModelForm):
     model = User
     fields = ('email', 'first_name', 'last_name', 'phone')
     # exclude = ('is_active')
 
-class UserRegisterForm(forms.ModelForm):
+class UserRegisterForm(StyleFormMixin, forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Повторите пароль' , widget=forms.PasswordInput)
 
@@ -23,12 +31,12 @@ class UserRegisterForm(forms.ModelForm):
         return cd['password2']
 
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(StyleFormMixin, forms.Form):
     email = forms.EmailField(label='email')
     password = forms.CharField(label='пароль', widget=forms.PasswordInput)
 
 
-class UserUpdateForm(forms.ModelForm):
+class UserUpdateForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'telegram', 'avatar')
