0743587bba8d1327be0a72b435e7940dec1468c2
Yura Simonov <yusimo@yandex.ru>
2026-02-18 00:08:01 +0300

    - Добавлен и проверен сброс пароля

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.shortcuts import render, redirect
 from django.urls.base import reverse
 from django.http import HttpResponseRedirect ,HttpResponse
@@ -6,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserChangePasswordForm
-
+from users.services import send_register_email, send_new_password
 
 def user_register_view(request):
     if request.method == 'POST':
@@ -17,6 +20,7 @@ def user_register_view(request):
             # print(form.cleaned_data['password'])
             new_user.set_password(form.cleaned_data['password'])
             new_user.save()
+            send_register_email(new_user.email)
             return HttpResponseRedirect(reverse('users:user_login'))
     context = {
         'title': 'Создать аккаунт',
@@ -104,6 +108,13 @@ def user_logout_view(request):
     logout(request)
     return redirect('dogs:index')
 
+@login_required(login_url='users:user_login')
+def user_generate_new_password_view(request):
+    new_password = ''.join(random.sample(string.ascii_letters + string.digits, k=12))
+    request.user.set_password(new_password)
+    request.user.save()
+    send_new_password(request.user.email, new_password)
+    return redirect(reverse('dogs:index'))
 
 
 
