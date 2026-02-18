b4bd2255b997415f74c425b23589ceffabfdfbdd
Yura Simonov <yusimo@yandex.ru>
2026-02-18 23:40:56 +0300

    - Расширена команда создания пользователей -Обновлена фикстура пользователей - Созданы и применены миграции

--- users/management/commands/ccsu.py
+++ users/management/commands/ccsu.py
@@ -5,14 +5,41 @@ from users.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        admin_user = User.objects.create(
-            email='admin@web.top',
-            first_name='Admin',
-            last_name='Adminov',
-            is_staff=True,
-            is_superuser=True,
-            is_active=True
-        )
-        admin_user.set_password('qwerty')
-        admin_user.save()
-        print('Admin Created')
+        users = {
+            'admin': {
+                'email': 'admin@web.top',
+                'first_name': 'Admin',
+                'last_name': 'Adminov',
+                'is_staff': True,
+                'is_superuser': True,
+                'is_active': True,
+                },
+            'moderator': {
+                'email': 'moderator@web.top',
+                'first_name': 'Moder',
+                'last_name': 'Moderov',
+                'is_staff': True,
+                'is_superuser': False,
+                'is_active': True,
+            },
+            'user': {
+                'email': 'user@web.top',
+                'first_name': 'User',
+                'last_name': 'Userov',
+                'is_staff': False,
+                'is_superuser': False,
+                'is_active': True,
+            },
+        }
+        for user, user_params in users.items():
+            cr_user = User.objects.create(
+                email=user_params['email'],
+                first_name=user_params['first_name'],
+                last_name=user_params['last_name'],
+                is_staff=user_params['is_staff'],
+                is_superuser=user_params['is_superuser'],
+                is_active=user_params['is_active']
+            )
+            cr_user.set_password('qwerty')
+            cr_user.save()
+            print(f'{user} Created')
