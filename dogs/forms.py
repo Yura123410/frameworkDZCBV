884cac486c9effb774464a3ddb7c726af68dc341
Yura Simonov <yusimo@yandex.ru>
2026-02-18 20:47:16 +0300

    - Добавлена валидация даты рождения собаки

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from datetime import datetime
 from dogs.models import Dog
 from users.forms import StyleFormMixin
 
@@ -8,3 +8,10 @@ class DogForm(StyleFormMixin, forms.ModelForm):
         model = Dog
         # fields = '__all__'
         exclude = ('owner',)
+
+    def clean_birth_date(self):
+        cleaned_data = self.cleaned_data['birth_data']
+        now_year = datetime.now().year
+        if now_year - cleaned_data.year > 32:
+            raise forms.ValidationError('Собака должна быть моложе 32-х лет')
+        return cleaned_data
