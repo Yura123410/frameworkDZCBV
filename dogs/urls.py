ec78676049fbc01b5f52c9f77b72be44577c7e85
Yura Simonov <yusimo@yandex.ru>
2026-02-20 00:07:03 +0300

    - dogs_list_view переписан на CBV - Внесены изменения в URL - Корректировки в старый views и шаблонах - обновлена фикстура

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dogs.views import index, breeds_list_view, breeds_dogs_list_view, dogs_list_view, dog_create_view, dog_detail_view, \
+from dogs.views import index, breeds_list_view, breeds_dogs_list_view, DogsListView, dog_create_view, dog_detail_view, \
     dog_update_view, dog_delete_view
 from dogs.apps import DogsConfig
 
@@ -12,7 +12,7 @@ urlpatterns = [
     path('breeds/<int:pk>/dogs/', breeds_dogs_list_view, name='breeds_dogs'),
 
     # dogs
-    path('dogs/', dogs_list_view, name='dogs_list'),
+    path('dogs/', DogsListView.as_view(), name='dogs_list'),
     path('dogs/create/', dog_create_view, name='dog_create'),
     path('dogs/detail/<int:pk>/', dog_detail_view, name='dog_detail'),
     path('dogs/update/<int:pk>/', dog_update_view, name='dog_update'),
--- dogs/views.py
+++ dogs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
 from dogs.models import Breed, Dog
@@ -10,7 +11,7 @@ from users.services import send_dog_creation
 
 def index(request):
     context = {
-        'objects_list': Breed.objects.all()[:3],
+        'object_list': Breed.objects.all()[:3],
         'title': 'Питомник - Главная'
     }
     return render(request, 'dogs/index.html', context)
@@ -18,7 +19,7 @@ def index(request):
 
 def breeds_list_view(request):
     context = {
-        'objects_list': Breed.objects.all(),
+        'object_list': Breed.objects.all(),
         'title': 'Питомник - Все наши породы'
     }
     return  render(request, 'dogs/breeds.html', context)
@@ -27,19 +28,25 @@ def breeds_list_view(request):
 def breeds_dogs_list_view(request, pk: int):
     breeds_item = Breed.objects.get(pk=pk)
     context = {
-        'objects_list': Dog.objects.filter(breed_id=pk),
+        'object_list': Dog.objects.filter(breed_id=pk),
         'title': f'Собаки породы - {breeds_item}',
         'breed_pk': breeds_item.pk,
     }
     return render(request, 'dogs/dogs.html', context)
 
-
-def dogs_list_view(request):
-    context = {
-        'objects_list': Dog.objects.all(),
+class DogsListView(ListView):
+    model = Dog
+    extra_context = {
         'title': 'Питомник все наши собаки'
     }
-    return render(request, 'dogs/dogs.html', context)
+    template_name = 'dogs/dogs.html'
+
+# def dogs_list_view(request):
+#     context = {
+#         'object_list': Dog.objects.all(),
+#         'title': 'Питомник все наши собаки'
+#     }
+#     return render(request, 'dogs/dogs.html', context)
 
 @login_required(login_url='users:user_login')
 def dog_create_view(request):
